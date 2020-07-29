f1f782606fc04007f173d8de4781484294952aae
lovroglogar <lovroglogar7@gmail.com>
2020-07-29 09:17:17 +0200

    Dont press me button fixed

--- app/src/main/java/com/example/jarvis/HomeFragment.java
+++ app/src/main/java/com/example/jarvis/HomeFragment.java
@@ -70,7 +70,6 @@ public class HomeFragment extends Fragment {
                 startActivity(intent);
             }
         });
-        enableButtons();
 
         setUpWelcomeText();
         loadAllTasks();
