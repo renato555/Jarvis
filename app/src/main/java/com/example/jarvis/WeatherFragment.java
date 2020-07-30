d3d449cc026f0da6f50c9794aa8a525a6389b616
renato555 <55023358+renato555@users.noreply.github.com>
2020-07-30 07:49:17 +0200

    nista

--- app/src/main/java/com/example/jarvis/WeatherFragment.java
+++ app/src/main/java/com/example/jarvis/WeatherFragment.java
@@ -2,10 +2,6 @@ package com.example.jarvis;
 
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
@@ -15,21 +11,16 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import com.google.android.material.button.MaterialButton;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 
@@ -45,8 +36,6 @@ public class WeatherFragment extends Fragment {
     private ImageView dayAfterTomorrowImage;
     private TextView dayAfterTomorrowText;
     private Button reloadButton;
-
-    //private static Bundle savedState;
 
     private OnSwipeTouchListener swipeListener;
 
@@ -67,9 +56,6 @@ public class WeatherFragment extends Fragment {
         });
 
         loadViews(view);
-//        if (savedState != null)
-//            restoreState();
-//        else
         loadWeather();
 
         return view;
@@ -79,7 +65,6 @@ public class WeatherFragment extends Fragment {
     public void onDestroy() {
         super.onDestroy();
         System.out.println("Destroy");
-        //saveState();
     }
 
     public void loadViews(View view) {
@@ -95,76 +80,8 @@ public class WeatherFragment extends Fragment {
     }
 
     public void loadWeather(){
-        //pleaseWait();
         startLoading(getView());
     }
-
-//    public void saveState() {
-//        savedState = new Bundle();
-//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//        Bitmap bitmap;
-//
-//        //Check if it didn't finish loading
-//        if(((BitmapDrawable) dayAfterTomorrowImage.getDrawable()).getBitmap().equals(((BitmapDrawable) getResources().getDrawable(R.drawable.ic_white_screen)).getBitmap())){
-//            savedState = null;
-//            return;
-//        }
-//
-//        //Save Today State
-//        savedState.putString(Constants.TODAY_KEY, todayText.getText().toString());
-//        bitmap = ((BitmapDrawable) todayImage.getDrawable()).getBitmap();
-//        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
-//        savedState.putByteArray(Constants.TODAY_IMAGE_KEY, baos.toByteArray());
-//        savedState.putString(Constants.TODAY_DESCRIPTION_KEY, todayDescription.getText().toString());
-//
-//        //Save Tomorrow State
-//        baos = new ByteArrayOutputStream();
-//        savedState.putString(Constants.TOMORROW_KEY, tomorrowText.getText().toString());
-//        bitmap = ((BitmapDrawable) tomorrowImage.getDrawable()).getBitmap();
-//        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
-//        savedState.putByteArray(Constants.TOMORROW_IMAGE_KEY, baos.toByteArray());
-//        savedState.putString(Constants.TOMORROW_DESCRIPTION_KEY, tomorrowDescription.getText().toString());
-//
-//        //Save Day After Tomorrow state
-//        baos = new ByteArrayOutputStream();
-//        savedState.putString(Constants.DAY_AFTER_TOMORROW_KEY, dayAfterTomorrowText.getText().toString());
-//        bitmap = ((BitmapDrawable) dayAfterTomorrowImage.getDrawable()).getBitmap();
-//        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
-//        savedState.putByteArray(Constants.DAY_AFTER_TOMORROW_IMAGE_KEY, baos.toByteArray());
-//        savedState.putString(Constants.DAY_AFTER_TOMORROW_DESCRIPTION_KEY, dayAfterTomorrowDescription.getText().toString());
-//    }
-
-//    public void restoreState() {
-//        Bitmap bitmap;
-//        byte[] byteArray;
-//
-//        //Restore Today State
-//        todayText.setText(savedState.get(Constants.TODAY_KEY).toString());
-//        byteArray = ((byte[]) savedState.get(Constants.TODAY_IMAGE_KEY));
-//        bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
-//        todayImage.setImageBitmap(Bitmap.createBitmap(bitmap));
-//        todayDescription.setText(savedState.get(Constants.TODAY_DESCRIPTION_KEY).toString());
-//
-//        // Restore Tomorrow state
-//        tomorrowText.setText(savedState.get(Constants.TOMORROW_KEY).toString());
-//        byteArray = ((byte[]) savedState.get(Constants.TOMORROW_IMAGE_KEY));
-//        bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
-//        tomorrowImage.setImageBitmap(Bitmap.createBitmap(bitmap));
-//        tomorrowDescription.setText(savedState.get(Constants.TOMORROW_DESCRIPTION_KEY).toString());
-//
-//        //Restore Day After Tomorrow state
-//        dayAfterTomorrowText.setText(savedState.get(Constants.DAY_AFTER_TOMORROW_KEY).toString());
-//        byteArray = ((byte[]) savedState.get(Constants.DAY_AFTER_TOMORROW_IMAGE_KEY));
-//        bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
-//        dayAfterTomorrowImage.setImageBitmap(Bitmap.createBitmap(bitmap));
-//        dayAfterTomorrowDescription.setText(savedState.get(Constants.DAY_AFTER_TOMORROW_DESCRIPTION_KEY).toString());
-//    }
-
-//    public void pleaseWait(){
-//        int waitForLenght = 2;
-//        for(int i = 0; i < waitForLenght; i++)
-//            Toast.makeText(getContext(), "Please wait...", Toast.LENGTH_LONG).show();
-//    }
 
     public void startLoading(View view) {
         new MyAsyncTask(todayText, todayDescription, todayImage, Constants.TODAY_IMAGE_CLASS_STRING, 0).execute();
