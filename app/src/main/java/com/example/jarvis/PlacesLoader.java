b5b84fab9881fc90e649c59329c46dbd577d1c24
lovroglogar <lovroglogar7@gmail.com>
2020-08-02 08:36:40 +0200

    Weather.3

---
+++ app/src/main/java/com/example/jarvis/PlacesLoader.java
@@ -0,0 +1,18 @@
+package com.example.jarvis;
+
+import android.content.Context;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlacesLoader {
+
+    public Map<String, String> loadPlaces(Context context){
+        Map<String, String> returnMap = new HashMap<>();
+
+        returnMap.put(context.getResources().getString(R.string.Zagreb), Constants.ZAGREB);
+        returnMap.put(context.getResources().getString(R.string.Bistra), Constants.BISTRA);
+
+        return returnMap;
+    }
+}
